9746d88b91b6cf4df39bc378afe75a697ef33aa9
canersu <canersu34@gmail.com>
2022-11-01 23:24:15 +0200

    remove torch model

--- ts_detection/src/ae.py
+++ ts_detection/src/ae.py
@@ -56,36 +56,6 @@ class autoEncoder:
         if printSummary:
             ae.summary()
         return ae
-    def torch_model1(self, printSummary=False):
-        self.encoder = nn.Sequential(
-            nn.Linear(48 * 48 * 3, 46*46*8),
-            nn.ReLU(),
-            nn.Linear(46*46*8, 44*44*16),
-            nn.ReLU(),
-            nn.MaxPool2d(2, stride=2),
-            nn.Linear(22*22*16, 20*20*32),
-            nn.ReLU(),
-            nn.Linear(20*20*32, 18*18*64),
-            nn.ReLU(),
-            nn.MaxPool2d(2, stride=2),
-            nn.Linear(5184, 1024),
-            nn.Linear(1024, 256),
-            nn.Linear(256, 1024),
-            nn.Linear(1024, 5184),
-            reshape()
-            nn.Upsample(size=(2,2)),
-            nn.ConvTranspose2d(5184, 18*18*64),
-            nn.ReLU(),
-
-            nn.ReLU(),
-            nn.ReLU(),
-            nn.ReLU(),
-
-
-
-
-            nn.Linear(18, 9)
-        )
 
     def model2(self, printSummary=False):
         ae = tf.keras.models.Sequential()
