fc6b38871ac712f4cf9d48073a687ecaec929ea3
canersu <canersu34@gmail.com>
2022-11-16 23:51:10 +0200

    todo..

--- notebooks/ae.py
+++ notebooks/ae.py
@@ -99,6 +99,8 @@ class autoEncoder:
         
     def SSIMLoss(self, y_true, y_pred):
         return 1 - tf.reduce_mean(tf.image.ssim(y_true, y_pred,1.0))
+
+    # TODO: Add draw graph method for comparison
        
     
     def MSEComparison(self, y_true, y_pred):
